949427992eda6ee6d7827546b8ef0f230171302a
Mohitthakur25 <156560529+Mohitthakur25@users.noreply.github.com>
2024-01-17 10:49:45 -0500

    Created script files
    Printed some sentences from Zen of Python.

---
+++ Script1.py
@@ -0,0 +1,2 @@
+print("Exploring the git and github for the first time.")
+print("I hope, that I will get pro in it.")
---
+++ Script2.py
@@ -0,0 +1,2 @@
+print("I found scripting application an interesting subject.")
+print("Looking forward to do well in it.")
