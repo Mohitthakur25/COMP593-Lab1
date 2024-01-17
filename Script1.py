252a20527ac81b008c0915001990452aef10fdc4
Mohitthakur25 <156560529+Mohitthakur25@users.noreply.github.com>
2024-01-17 11:45:22 -0500

    modified both files in the repo clone
    deleted a print() statement from script1.py
    added two print() statements to script2.py

--- Script1.py
+++ Script1.py
@@ -1,4 +1,4 @@
 #printing some sentences for script 1 
 
 print("Exploring the git and github for the first time.")
-print("I hope, that I will get pro in it.")
+
--- Script2.py
+++ Script2.py
@@ -1,3 +1,6 @@
 #printing some sentences for script 2
 print("I found scripting application an interesting subject.")
 print("Looking forward to do well in it.")
+print("Jay shree Ram")
+print("Ram Ram")
+
